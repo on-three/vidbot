a0bb72b2f99d458bc30a276536428dcdb47b19aa
on-three <on_three@outlook.com>
2015-08-12 01:17:30 -0700

    baic video controls work.

--- vidbot.js
+++ vidbot.js
@@ -1,10 +1,27 @@
-var mpv = require('./lib/mpv.js').Mpv;
+var M = require('./lib/mpv.js');
+
+var channel = function(something) {
+  console.log(something);
+}
 
 if(process.argv.length<3) {
   console.error("USAGE: vidbot <input video file or url>");
 }
 var vid = process.argv[2]
 
-var m = new mpv(vid,null);
-setTimeout(m.time_remaining, 5000);
-setTimeout(m.time_remaining, 10000);
+//var m = new mpv(vid, channel);
+//m.test();
+//setTimeout(m.time_remaining, 5000);
+//setTimeout(m.filename, 10000);
+//setTimeout(m.pause, 15000);
+
+var m = new M(vid, channel);
+setTimeout(function(){m.time_remaining();}, 5000);
+setTimeout(function(){m.filename();}, 6000);
+setTimeout(function(){m.pause(true);}, 7000);
+setTimeout(function(){m.pause(false);}, 8000);
+setTimeout(function(){m.mute(true);}, 9000);
+setTimeout(function(){m.mute(false);}, 10000);
+setTimeout(function(){m.quit();}, 15000);
+
+
