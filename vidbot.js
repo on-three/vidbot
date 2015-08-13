fa91370434fe90065558d96607acb88ef317518b
on-three <on_three@outlook.com>
2015-08-12 22:53:57 -0700

    Added IRC interface.

--- vidbot.js
+++ vidbot.js
@@ -1,27 +1,75 @@
-var M = require('./lib/mpv.js');
+var irc = require('irc');
+var config = require('./settings.json');
 
-var channel = function(something) {
-  console.log(something);
+//plugins
+var plugins = [
+  require('./lib/youtube.js')
+  ]
+
+// Command line arguments
+// if(process.argv.length<3) {
+//   console.error("USAGE: vidbot <input video file or url>");
+// }
+
+
+var Channel = function(name, client) {
+  this.client = client;
+  this.name = name;
+  this.say = function(something) {
+    client.say(name, something);
+  }
+  this.log = function(something) {
+    console.log(something);
+  }
 }
 
-if(process.argv.length<3) {
-  console.error("USAGE: vidbot <input video file or url>");
+function handleMessage(channel, from, msg) {
+  var message = msg;
+  var arrayLength = plugins.length;
+  for (var i = 0; i < arrayLength; i++) {
+    message = plugins[i].handleMessage(channel, from, msg);
+    if(!message) {
+      return message;
+    }
+  }
+  return message;
 }
-var vid = process.argv[2]
 
-//var m = new mpv(vid, channel);
-//m.test();
-//setTimeout(m.time_remaining, 5000);
-//setTimeout(m.filename, 10000);
-//setTimeout(m.pause, 15000);
+var bot = new irc.Client(config.server, config.nick, {
+    channels: [config.channel],
+    port: config.port,
+});
 
-var m = new M(vid, channel);
-setTimeout(function(){m.time_remaining();}, 5000);
-setTimeout(function(){m.filename();}, 6000);
-setTimeout(function(){m.pause(true);}, 7000);
-setTimeout(function(){m.pause(false);}, 8000);
-setTimeout(function(){m.mute(true);}, 9000);
-setTimeout(function(){m.mute(false);}, 10000);
-setTimeout(function(){m.quit();}, 15000);
+var channel = new Channel(config.channel, bot);
+
+bot.addListener('pm', function (from, message) {
+    console.log(from + ' => ME: ' + message);
+});
+
+bot.addListener('error', function(message) {
+    console.log('error: ', message);
+});
+
+bot.addListener('pm', function(nick, message) {
+    console.log('Got private message from %s: %s', nick, message);
+});
+
+bot.addListener('join', function(c, who) {
+    console.log('%s has joined %s', who, c);
+});
+
+bot.addListener('part', function(c, who, reason) {
+    console.log('%s has left %s: %s', who, c, reason);
+});
+
+bot.addListener('kick', function(c, who, by, reason) {
+    console.log('%s was kicked from %s by %s: %s', who, c, by, reason);
+});
+
+bot.addListener('message'+config.channel, function (from, message) {
+    console.log(from + ' => '+ config.channel+' ' + message);
+    handleMessage(channel, from, message);
+});
+
 
 
